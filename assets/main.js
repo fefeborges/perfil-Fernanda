006809f15a08705550689a564168f976279727ca
fefeborges <fernandavargas1011@gmail.com>
2023-08-04 11:35:01 -0300

    Finalização das alterações

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,6 @@
 function mudacor() 
 {
     document.body.style.backgroundColor = 'lightyellow';
+    document.getElementById( 'flor1' ).style.visibility = "visible";
+    document.getElementById( 'flor2' ).style.visibility = "visible";
 }
